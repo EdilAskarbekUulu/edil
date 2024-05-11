6bc10db5a24eec0732db8ead88bb2a4360f21fb3
EdilAskarbekUulu <edilaskarov55@gmail.com>
2024-05-11 21:27:19 +0600

    edil
    Signed-off-by: EdilAskarbekUulu <edilaskarov55@gmail.com>

--- my-app/src/cotolog/catologAdd.jsx
+++ my-app/src/cotolog/catologAdd.jsx
@@ -1,35 +1,28 @@
 import React from "react";
 import './cotologAdd.css'
+import photo1 from './img/right.png'
 
 export default function CatologAdd(){
     return(
         <>
         <div className="catologAdd">
             <ul>    
-                <li>Щведские стенки</li>
-                <li>Турники</li>
-                <li>Уличные комплексы</li>
-                <li>Тяжелая атлетика
-                    <ul>
-                        <li>Силовые скамьи и стойки</li>
-                        <li>Мультистанции</li>
-                        <li>Тренажеры для ног</li>
-                        <li>Силовые рамы</li>
-                        <li>Турники и брусья</li>
-                    </ul>
-                </li>
-                <li>Стойки под штангу</li>
-                <li>Единоборства</li>
-                <li>Фитнес</li>
-                <li>Единоборства</li>
-                <li>Тубинги</li>
-                <li>Батуты</li>
-                <li>Бассейны</li>
-                <li>Рюкзаки и сумки</li>
-                <li>Новогодние ёлки</li>
-                <li>Детские игрушки</li>
-                <li>Самокаты</li>
-                <li>Велосипеды</li>
+                <li>Щведские стенки <img src={photo1} className="photoCat" /></li>
+                <li>Турники <img src={photo1} className="photoCat" /></li>
+                <li>Уличные комплексы <img src={photo1} className="photoCat" /></li>
+                <li>Тяжелая атлети <img src={photo1} className="photoCat" /></li>
+                <li>Стойки под штангу <img src={photo1} className="photoCat" /></li>
+                <li>Единоборства <img src={photo1} className="photoCat" /></li>
+                <li>Фитнес <img src={photo1} className="photoCat" /></li>
+                <li>Единоборства <img src={photo1} className="photoCat" /></li>
+                <li>Тубинги <img src={photo1} className="photoCat" /></li>
+                <li>Батуты <img src={photo1} className="photoCat" /></li>
+                <li>Бассейны <img src={photo1} className="photoCat" /></li>
+                <li>Рюкзаки и сумки <img src={photo1} className="photoCat" /></li>
+                <li>Новогодние ёлки <img src={photo1} className="photoCat" /></li>
+                <li>Детские игрушки <img src={photo1} className="photoCat" /></li>
+                <li>Самокаты <img src={photo1} className="photoCat" /></li>
+                <li>Велосипеды <img src={photo1} className="photoCat" /></li>
             </ul>
         </div>
         </>
