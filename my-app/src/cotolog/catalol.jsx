163f52b7ab62ed5e114fcd6d16094e93ce021f93
EdilAskarbekUulu <edilaskarov55@gmail.com>
2024-05-11 20:59:20 +0600

    'dil
    Signed-off-by: EdilAskarbekUulu <edilaskarov55@gmail.com>

--- my-app/src/cotolog/catalol.jsx
+++ my-app/src/cotolog/catalol.jsx
@@ -1,15 +1,26 @@
 import React from "react";
+import { useState } from "react";
+import CatologAdd from "./catologAdd";
 import './cotolog.css'
 import photo10 from "./img/burger-menu.png"
 import photo11 from "./img/down.png"
 
 export default function Cotolog(){
+    const[add, setadd]=useState(false)
+
+    function addUl(){
+        setadd(!add)
+    }
+    
     return(
         <>
         <section className="sectionOne">
             <div className="cotologMain">
                 <div className="cotologWrap">
-                    <button className="cotologBtn"><img src={photo10}/>Каталог товаров <img src={photo11} /></button>
+                    <button className="cotologBtn" onClick={addUl}><img src={photo10}/>Каталог товаров <img src={photo11} /></button>
+                    {add && (
+                        <CatologAdd/>
+                    )}
                     <ul className="cotologUl">
                         <li>Шведские стенки</li>
                         <li>Уличные комплексы</li>
---
+++ my-app/src/cotolog/catologAdd.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import './cotologAdd.css'
+
+export default function CatologAdd(){
+    return(
+        <>
+        <div className="catologAdd">
+            <ul>    
+                <li>Щведские стенки</li>
+                <li>Турники</li>
+                <li>Уличные комплексы</li>
+                <li>Тяжелая атлетика
+                    <ul>
+                        <li>Силовые скамьи и стойки</li>
+                        <li>Мультистанции</li>
+                        <li>Тренажеры для ног</li>
+                        <li>Силовые рамы</li>
+                        <li>Турники и брусья</li>
+                    </ul>
+                </li>
+                <li>Стойки под штангу</li>
+                <li>Единоборства</li>
+                <li>Фитнес</li>
+                <li>Единоборства</li>
+                <li>Тубинги</li>
+                <li>Батуты</li>
+                <li>Бассейны</li>
+                <li>Рюкзаки и сумки</li>
+                <li>Новогодние ёлки</li>
+                <li>Детские игрушки</li>
+                <li>Самокаты</li>
+                <li>Велосипеды</li>
+            </ul>
+        </div>
+        </>
+    )
+}
