c3db2758517bca39f44a29d26f49a01e31815ab2
EdilAskarbekUulu <edilaskarov55@gmail.com>
2024-05-13 16:32:01 +0600

    FIX
    Signed-off-by: EdilAskarbekUulu <edilaskarov55@gmail.com>

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,6 +3,10 @@ import HeaderOne from "./components/headerOne/headerOne";
 import HeaderTwo from "./components/headerTwo/headerTwo";
 import Desires from "./components/desires/desires";
 import Shvetcia from "./components/shvetcia/shvetcia";
+import Company from "./components/Company/Company";
+import Payment from "./components/Payment/Payment";
+import Email from "./components/Email/Email";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
@@ -12,6 +16,10 @@ function App() {
       <Cotolog/>
       <Desires/>
       <Shvetcia/>
+      <Company/>
+      <Payment/>
+      <Email/>
+      <Footer/>
     </div>
   );
 }
